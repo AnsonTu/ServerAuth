a55ae623b1998c0ee2409bc362d45899a5120d2d
Anson Tu <anson9137@gmail.com>
2019-08-21 14:21:46 -0400

    Added route for signup

---
+++ controllers/authentication.js
@@ -0,0 +1,4 @@
+// Export function for `/signup` route to return
+exports.signup = function(req, res, next) {
+  res.send({ success: "true" });
+};
--- router.js
+++ router.js
@@ -1,10 +1,13 @@
+const Authentication = require("./controllers/authentication");
+
 // Export a functuon in node
 module.exports = function(app) {
   // Route for root path
   // req (request) is the incoming http request
   // res (response) is the response that is returned to the user
   // next is used for error handling
-  app.get("/", function(req, res, next) {
+  /*app.get("/", function(req, res, next) {
     res.send(["water bottle", "phone", "paper"]);
-  });
+  });*/
+  app.post("/signup", Authentication.signup);
 };
