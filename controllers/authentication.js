47aa3dce5926cd5486c332ad94237a5f153241b6
Anson Tu <anson9137@gmail.com>
2019-09-16 10:00:07 -0400

    Create and return JWT for auth

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -1,4 +1,11 @@
+const jwt = require("jwt-simple");
 const User = require("../models/user");
+const config = require("../config");
+
+function tokenForUser(user) {
+  const timestamp = new Date().getTime();
+  return jwt.encode({ sub: user.id, iat: timestamp }, config.secret);
+}
 
 // Export function for `/signup` route to return
 exports.signup = function(req, res, next) {
@@ -9,7 +16,7 @@ exports.signup = function(req, res, next) {
   if (!email || !password) {
     return res
       .status(422)
-      .send({ error: "You must provide an email and passowrd" });
+      .send({ error: "You must provide an email and password" });
   }
 
   // Check if a user with the given email exists
@@ -35,7 +42,7 @@ exports.signup = function(req, res, next) {
       }
 
       // Respond to the request indicating the user was created
-      res.json({ success: true });
+      res.json({ token: tokenForUser(user) });
       /* Return user will return the user's email and password
       res.json(user); */
     });
