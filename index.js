04e1e82c06b325575fa60e5d26e8d26fbf44c42c
Anson Tu <anson9137@gmail.com>
2019-10-15 15:42:20 -0400

    Configured server avoid CORS errors

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const morgan = require("morgan");
 const app = express();
 // Add new routes
 const router = require("./router");
+const cors = require("cors");
 
 // Set up the connection between mongoose and mongoDB
 const mongoose = require("mongoose");
@@ -14,10 +15,13 @@ mongoose.connect("mongodb://localhost:27017/auth");
 
 // App Setup
 // app.use() is used to register middleware
+// Using cors stops default browser CORS behaviour, allowing
+//  API calls from any domain, subdomain, and port
 // morgan is a logging middleware
 // bodyParser is a middleware used to parse incoming requests into JSON
 // nodemon automatically restarts the server whenver a change is made
 app.use(morgan("combined"));
+app.use(cors());
 app.use(bodyParser.json({ type: "*/*" }));
 router(app);
 
